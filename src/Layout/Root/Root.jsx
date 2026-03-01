83e625faaf6aeb6a8a231ee736d509c6543728a6
Syeed141 <shahariarsyeed2026@gmail.com>
2026-03-01 22:46:45 +0600

    Initial setup done

---
+++ src/Layout/Root/Root.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../Component/Navbar/Navbar';
+import Home from '../../Component/Home/Home';
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar> </Navbar>
+
+            <Home> </Home>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+
+// react router 
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './Layout/Root/Root.jsx';
+import Login from './Component/Login/Login.jsx';
+import Register from './Component/Register.jsx/Register.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+
+    children : [
+
+      {path : "Login", Component: Login },
+      {path : "Register", Component: Register  },
+
+
+    ]
+  },
+]);
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
